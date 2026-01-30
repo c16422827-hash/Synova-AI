69354c0c3dc34ec5d58f7c290c4b448fb2acad9e
c16422827-hash <c16422827-hash@users.noreply.github.com>
2026-01-30 06:53:55 -0500

    Remove duplicates and unnecessary files - cleanup project structure

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .message import Message
-
-# This makes the Message class available when importing from models package
-__all__ = ['Message']
--- start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Simple starter script for Windows
-python -m uvicorn main:app --host 0.0.0.0 --port 8000
-pause
-
